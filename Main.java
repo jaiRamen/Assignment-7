5261305035fe8665b31cda5ce221a00104c082fd
Jai B <68177769+jaiRamen@users.noreply.github.com>
2020-12-20 01:56:57 -0700

    Update Main.java
    adding comments etc

--- Main.java
+++ Main.java
@@ -1,10 +1,15 @@
+//Jai Breisch 1410 Week 7 Main Method
+
+//ShayneDarak contributed to this code by helping me use more efficient ways to declutter and reduce the amount of code
+
+
 import java.io.IOException;
 
 public class Main {
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
-
-		
+		//We now call in the Employee class to read each employee from file and make it into a 
+		//usable set of information by separating it into sets of data per each employee
 		Employee e1 = new Employee(1);
 		Employee e2 = new Employee(2);
 		Employee e3 = new Employee(3);
@@ -18,7 +23,8 @@ public class Main {
 		Employee e11 = new Employee(11);
 		Employee e12 = new Employee(12);
 		
-	
+	//Now the main class calls in all of the work done by the employee class as well as the formatting
+	//done by the printEmployee method, to finally compile all data from the CSV file, then print it nicely.
 		e1.printEmployee();
 		e2.printEmployee();
 		e3.printEmployee();
